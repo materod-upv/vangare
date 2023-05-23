e922ee78309286254f35b656234c41f21da70767
María Ten Rodríguez <materod@upv.edu.es>
2023-05-23 19:58:12 +0000

    Updated depdendencies

--- setup.py
+++ setup.py
@@ -4,46 +4,56 @@
 
 from setuptools import setup, find_packages
 
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    with open(filename) as f:
+        lineiter = [line.strip() for line in f]
+    return [line for line in lineiter if line and not line.startswith("#")]
+
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = parse_requirements("requirements.txt")
 
-test_requirements = ['pytest>=3', ]
+test_requirements = parse_requirements("requirements_dev.txt")
 
 setup(
+    name='vangare',
+    version='0.1.0',
+    description="Python XMPP Server",
+    long_description=readme + '\n\n' + history,
     author="María Ten Rodríguez",
     author_email='materod@upv.edu.es',
-    python_requires='>=3.6',
+    url='https://github.com/materod-upv/vangare',
+    python_requires='>=3.8',
+    packages=find_packages(include=['vangare', 'vangare.*']),
+    entry_points={
+        'console_scripts': [
+            'vangare=vangare.cli:main',
+        ],
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='vangare',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'Topic :: Internet :: XMPP',
     ],
-    description="Python XMPP Server",
-    entry_points={
-        'console_scripts': [
-            'vangare=vangare.cli:main',
-        ],
-    },
-    install_requires=requirements,
-    license="MIT license",
-    long_description=readme + '\n\n' + history,
-    include_package_data=True,
-    keywords='vangare',
-    name='vangare',
-    packages=find_packages(include=['vangare', 'vangare.*']),
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/materod-upv/vangare',
-    version='0.1.0',
-    zip_safe=False,
 )
